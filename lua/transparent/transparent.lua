99acef0fea8048fa2088f143d1ac8e4dac812ec3
Aotrix <aotrix56@gmail.com>
2024-02-16 22:11:55 +0100

    added telescope transparency

--- lua/transparent/transparent.lua
+++ lua/transparent/transparent.lua
@@ -1,7 +1,10 @@
 require("transparent").setup({
     extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
-    -- "TelescopeBorder",
-    "FloatBorder"
+    "FloatBorder",
+    "TelescopeBorder",
+    "TelescopeNormal",
+    "TelescopeSelection",
+    "TelescopeSelectionCaret"
 },
 })
