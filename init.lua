7b504093ae9c46c74ff4d580c7edc2e17377a1ea
Aotrix <aotrix56@gmail.com>
2024-06-22 19:33:06 +0200

    added coc.nvim

--- init.lua
+++ init.lua
@@ -21,27 +21,13 @@ require('lazy').setup({
     
     -- LSP
     {
-        {'VonHeikemen/lsp-zero.nvim',
+         -- Autocompletion
+        {'neoclide/coc.nvim' ,
         config = function()
             require('autocomplete/autocomplete')
-        end
+        end ,
+        branch="release"
         },
-        'neovim/nvim-lspconfig',
-
-
-         {'williamboman/mason.nvim',
-         config = function()
-            require('mason').setup()
-         end},
-         'williamboman/mason-lspconfig.nvim' , -- Optional
-
-         -- Autocompletion
-         'hrsh7th/nvim-cmp', -- Required
-         'hrsh7th/cmp-nvim-lsp', -- Required
-         'hrsh7th/cmp-buffer', -- Optional
-         'hrsh7th/cmp-path', -- Optional
-         'saadparwaiz1/cmp_luasnip', -- Optional
-         'hrsh7th/cmp-nvim-lua', -- Optional
 
          -- Snippets
          'L3MON4D3/LuaSnip' ,    -- Required
