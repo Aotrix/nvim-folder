a1cac65f6ce77a3a118cd912dfbacd0769e411ec
Aotrix <aotrix56@gmail.com>
2025-02-23 04:23:42 +0100

    autocompletion worksgit add -A

--- lua/aotrix/plugins/telescope.lua
+++ lua/aotrix/plugins/telescope.lua
@@ -6,10 +6,10 @@ return {
         'nvim-telescope/telescope-live-grep-args.nvim',
         'nvim-telescope/telescope-file-browser.nvim',
         {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+        'nvim-telescope/telescope-ui-select.nvim',
     },
 
     config = function()
-        require('telescope').load_extension('fzf')
         local actions = require("telescope.actions")
         require("telescope").setup({
             defaults = {
@@ -20,21 +20,24 @@ return {
                         -- map actions.which_key to <C-h> (default: <C-/>)
                         -- actions.which_key shows the mappings for your picker,
                         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-n>"] = actions.preview_scrolling_down,
-                        ["<C-p>"] = actions.preview_scrolling_up,
                         ["<esc>"] = actions.close,
                     }
                 }
             },
             extensions = {
-                fzf = {}
+                fzf = {},
+                ['ui-select'] = {
+                    require('telescope.themes').get_dropdown(),
+                },
             },
         })
 
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+
         vim.keymap.set("n", "<leader>pv", function() require("telescope").extensions.file_browser.file_browser() end)
         vim.keymap.set("n", "<leader>pf", function() require("telescope.builtin").find_files() end)
+        vim.keymap.set("n", "gd", function() require("telescope.builtin").lsp_definitions() end)
 
         require('aotrix.multigrep').setup()
     end
