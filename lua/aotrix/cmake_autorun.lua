f08d58ffb5b69fad37e5abee26be19dbcd8896c6
Aotrix <aotrix56@gmail.com>
2025-02-26 03:12:44 +0100

    added rg command to retrieve executable names from cmake build dir

--- lua/aotrix/cmake_autorun.lua
+++ lua/aotrix/cmake_autorun.lua
@@ -1,3 +1,8 @@
 -- use :!cmake --build PATH_TO_BUILD_FOLDER
 -- && Telescope to choose executable in PATH_TO_BUILD_FOLDER/EXECUTABLE_FOLDER/*
--- Then launch executable in a normal terminal - not nvim one
+-- Find these lines (in cmake.build) : agougou is the "bin" folder, program is the exe name
+-- CMakeFiles/program.dir/build: agougou/program
+-- rg 'CMakeFiles\/(.*).dir\/build:(?!.*Lib*)' build/CMakeFiles/ --no-filename -Por '$1'
+vim.keymap.set("n", "mt",
+               "<CMD>terminal cmake --build build && ./build/bin/* <CR>",
+               {noremap = true, silent = true})
